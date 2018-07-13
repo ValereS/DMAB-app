32885554e9bb92f9be79524b091aa5a20d92d4a6
val <me@forme.com>
2018-07-13 21:01:40 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dmab-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'dmab';
+}
